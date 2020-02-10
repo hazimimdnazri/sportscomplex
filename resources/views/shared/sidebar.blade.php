a34fe468414877c2f8b2e3fe1a2aaa8d41b6e014
Hazimi Md Nazri <hazimimdnazri@gmail.com>
2020-02-10 13:09:55 +0800

    123

--- resources/views/shared/sidebar.blade.php
+++ resources/views/shared/sidebar.blade.php
@@ -1,15 +1,5 @@
-<aside class="main-sidebar">
+<aside class="main-sidebar skin-blue-light">
     <section class="sidebar">
-    <div class="user-panel">
-        <div class="pull-left image">
-        <img src="{{ asset('assets/dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
-        </div>
-        <div class="pull-left info">
-        <p>{{ Auth::user()->name }}</p>
-        <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-        </div>
-    </div>
-
     <ul class="sidebar-menu" data-widget="tree">
         <li class="header">MAIN NAVIGATION</li>
         <li><a href="{{ url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
