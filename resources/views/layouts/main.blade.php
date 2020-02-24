dff3ad332595f06e6d560f8d4c4f92a51bb0f389
Hazimi Md Nazri <hazimimdnazri@gmail.com>
2020-02-24 12:16:44 +0800

    uat

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -39,12 +39,12 @@
 					<ul class="nav navbar-nav">
 					<li class="dropdown user user-menu">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-						<img src="{{ asset('assets/dist/img/user2-160x160.jpg') }}" class="user-image" alt="User Image">
+						<img src="{{ asset('assets/images/default.jpg') }}" class="user-image" alt="User Image">
 						<span class="hidden-xs">{{ Auth::user()->name }}</span>
 						</a>
 						<ul class="dropdown-menu">
 						<li class="user-header">
-							<img src="{{ asset('assets/dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
+							<img src="{{ asset('assets/images/default.jpg') }}" class="img-circle" alt="User Image">
 
 							<p>
 							{{ Auth::user()->name }}
