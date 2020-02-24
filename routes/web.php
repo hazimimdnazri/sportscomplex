d458bbe989e7e6bf5ff1e546a2be17e2a6769edd
Hazimi Md Nazri <hazimimdnazri@gmail.com>
2020-02-24 11:44:39 +0800

    uat

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 // Auth::routes();
-Route::get('/', 'HomeController@index');
+Route::get('/', 'HomeController@index')->middleware('auth');
 Route::get('unauthorized', 'HomeController@unauthorized');
 
 Route::get('login', 'Auth\LoginController@showLoginForm');
