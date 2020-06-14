6cc4471d73b7ead580c4052ee8d75b9fdd4d2fc5
Hazimi Md Nazri <hazimimdnazri@gmail.com>
2020-06-14 13:22:27 +0800

    123

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -16,64 +16,20 @@
 	</ol>
 </section>
 <section class="content">
-	<div class="row">
-		<div class="col-lg-3 col-xs-6">
-			<div class="small-box bg-aqua">
-				<div class="inner">
-					<h3>150</h3>
-					<p>Total Assets</p>
-				</div>
-				<div class="icon">
-					<i class="ion ion-bag"></i>
-				</div>
-				<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-			</div>
-		</div>
-		<div class="col-lg-3 col-xs-6">
-			<div class="small-box bg-green">
-				<div class="inner">
-					<h3>53<sup style="font-size: 20px">%</sup></h3>
-					<p>Assets In Use</p>
-				</div>
-				<div class="icon">
-					<i class="ion ion-stats-bars"></i>
-				</div>
-				<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-			</div>
-		</div>
-		<div class="col-lg-3 col-xs-6">
-			<div class="small-box bg-yellow">
-				<div class="inner">
-					<h3>44</h3>
-					<p>Total User</p>
-				</div>
-				<div class="icon">
-					<i class="ion ion-person-add"></i>
-				</div>
-				<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-			</div>
-		</div>
-		<div class="col-lg-3 col-xs-6">
-			<div class="small-box bg-red">
-				<div class="inner">
-					<h3>65</h3>
-					<p>Unique Visitors</p>
-				</div>
-				<div class="icon">
-					<i class="ion ion-pie-graph"></i>
-				</div>
-				<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-12">
+    <div class="row">
+        <div class="col-md-12">
             <div class="box box-primary">
                 <div class="box-body no-padding">
+					<div class="col-md-12 text-center" style="margin-top: 20px; margin-bottom: 20px;">
+						<h2>Coming Soon!</h2>
+					</div>
+					<div class="col-md-12">
+                    	<div id="calender"></div>
+					</div>
                 </div>
             </div>
         </div>
-	</div>
+    </div>
 </section>
 @endsection
 
